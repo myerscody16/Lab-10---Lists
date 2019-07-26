77685badeaab2ee2068ea262e5178332cb40b9b6
myerscody16 <myerscody16@live.com>
2019-07-25 21:58:09 -0400

    initial push

---
+++ Lab 10 Classes - Circle/Class1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Lab_10_Classes___Circle
+{
+    class Circle
+    {
+        #region Fields
+        private double radius;
+        private double circumference;
+        private double area;
+        #endregion
+
+        #region Properties
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        public double Circumference
+        {
+            get
+            {
+                return circumference;
+            }
+            set
+            {
+                circumference = value;
+            }
+        }
+        public double Area
+        {
+            get
+            {
+                return area;
+            }
+            set
+            {
+                area = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+        public Circle(double radius)
+        {
+
+        }
+
+        #endregion
+
+        #region Methods
+        public double CalculateCircumference()
+        {
+            radius = 6;
+            double circumference = Math.PI * 2 * radius;
+            return circumference;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            //extra credit
+        }
+        public double CalculateArea()
+        {
+            double area = Math.PI * radius * radius;
+            return area;
+        }
+        public string CalculateFormattedArea()
+        {
+            //extra credit
+        }
+
+
+        #endregion
+    }
+}
---
+++ Lab 10 Classes - Circle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab_10_Classes___Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get user input
+            Console.WriteLine("What is the radius of the circle?");
+            bool cont = true;
+            while(cont)
+            {
+                try
+                {
+                    double usrInput = double.Parse(Console.ReadLine());
+                    cont = false;
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input, please give a valid radius and then press enter");
+                    continue;
+                }
+            }
+            Circle circumference = Circle.CalculateCircumference();
+            Console.WriteLine(circumference);
+
+        }
+    }
+}
